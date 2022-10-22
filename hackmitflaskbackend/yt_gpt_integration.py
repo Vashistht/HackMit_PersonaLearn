84bb3b53bd19e520c8f818941d7b998749f1199f
Jacob Daitzman <jdtzmn@gmail.com>
2022-10-21 23:02:57 -0400

    Clean up `yt_gpt_integration.py`

--- hackmitflaskbackend/yt_gpt_integration.py
+++ hackmitflaskbackend/yt_gpt_integration.py
@@ -13,8 +13,8 @@ class YtRec:
         self.trascript = transcript
         self.ytapi = os.getenv('YOUTUBE_API_KEY')
         openai.api_key = os.getenv('OPENAI_API_KEY')
-        print(self.ytapi, openai.api_key )
-    
+
+
     def gpt_keywords(self):
         words = self.trascript
         query = "Extract keywords from this text: "
@@ -33,6 +33,7 @@ class YtRec:
         text_new = text.replace('\n-', ',')
         return text_new
 
+
     def gpt_topic(self):
         words = self.trascript
         query_new = "Find the topic:"
@@ -50,34 +51,43 @@ class YtRec:
         x_new = response_new
         text = x_new['choices'][0]['text']
         return text
-    
-    
+
+
     def youtube_get_videos(self):
-        #Initializing the API
+        # Initializing the API
         api_key = self.ytapi
         youtube = build( 'youtube', 'V3', developerKey= api_key)
         
         text = self.gpt_topic()
-        #Searching for query with decreasing order as viewcount and result as video type
-        results = youtube. search().list(q=text, part="snippet", type="video", 
-                                         order="viewCount").execute( )
+
+        # Searching for query with decreasing order as viewcount and result as video type
+        results = youtube. search().list(
+            q=text,
+            part="snippet",
+            type="video",
+            order="viewCount"
+        ).execute()
         
         return results
-    
-    # this next few fns can def be better
+
+
+    # Getters
     def get_title(self, item):
         title = item['snippet']['title']
         return title
+
 
     def get_thumbnail(self, item):
         video_id = item['id']['videoId']
         video_url = "https://img.youtube.com/vi/" + video_id + "/default.jpg"
         return video_url
 
+
     def get_video_url(self, item):
         video_id = item['id']['videoId']
         video_thumbnail = "https://www.youtube.com/watch?v=" + video_id
         return video_thumbnail
+
 
     def get_viewcount(self, item, youtube):
         video_id = item['id']['videoId']
@@ -86,52 +96,61 @@ class YtRec:
         viewcount = int(video_statistics['items'][0]['statistics']['viewCount'])
         return viewcount
 
+
     def get_likecount(self, item, youtube):
         video_id = item['id']['videoId']
         video_statistics = youtube.videos().list(id=video_id,
                                             part='statistics').execute()
         likecount = int(video_statistics['items'][0]['statistics']['likeCount'])
         return likecount
-    
-    # refine this based on the feedback from the team 
-    def custom_score(self, likecount, viewcount) :
+
+    # Scoring and ordering
+    def custom_score(self, likecount, viewcount):
+        """Score for a video based on the like count and view count."""
         score = ( (likecount **2 + viewcount) / (viewcount))
         return score
 
+
     def new_order(self, results, df, youtube):
-        i = 1
         topics, keywords = self.gpt_topic(), self.gpt_keywords()
-        for item in results['items']:
+
+        for i, item in enumerate(results['items']):
             title = self.get_title(item)
             thumbnail = self.get_thumbnail(item)
             viewcount = self.get_viewcount(item, youtube)
             video_url = self.get_video_url(item)
             likecount = self.get_likecount(item, youtube)
-            # dislikecount = get_dislikecount(item, youtube)
             score = self.custom_score(likecount, viewcount)
-            df.loc[i] = [title, thumbnail,video_url, topics, keywords, score, viewcount, ]
-            i += 1
+            df.loc[i + 1] = [title, thumbnail,video_url, topics, keywords, score, viewcount]
+
         df = df.sort_values(['custom_score'], ascending=[0])
         df = df.reset_index(drop=True)
+
         return df
-    ##### back to okay code
-    
+
+
+    # Dataframes and dictionaries
     def final_df(self):
         # Initialise results dataframe
         df = pd.DataFrame(columns=('title', 'thumbnail', 'url', 'topics', 
-                                   'keywords', 'custom_score', 'views'))
+            'keywords', 'custom_score', 'views'))
         api_key = self.ytapi
         print(api_key)
-        youtube = build( 'youtube', 'V3', developerKey= api_key)
+        youtube = build('youtube', 'V3', developerKey=api_key)
         
         text = self.gpt_topic()
         #Searching for query with decreasing order as viewcount and result as video type
-        results = youtube. search().list(q=text, part="snippet", type="video", 
-                                         order="viewCount").execute( )
+        results = youtube.search().list(
+            q=text,
+            part="snippet",
+            type="video", 
+            order="viewCount"
+        ).execute( )
         
         df_final = self.new_order(results, df, youtube)
         return df_final
-    
+
+
     def final_dict(self):
         df_final = self.final_df()
         df_final = df_final.drop(columns = ['custom_score','views'])
